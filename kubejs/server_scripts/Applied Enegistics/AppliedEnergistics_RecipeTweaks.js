00f047217684984e8b6c10183282d5bc3cefefe5
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-03-31 03:39:23 -0500

    Adjusted AE2 Recipes to be more fair and use new Create Post Production Items

--- kubejs/server_scripts/Applied Enegistics/AppliedEnergistics_RecipeTweaks.js
+++ kubejs/server_scripts/Applied Enegistics/AppliedEnergistics_RecipeTweaks.js
@@ -1,104 +1,106 @@
 ServerEvents.recipes(event => {
 
 // Removed OP Item in Relation to Create
-event.remove({ id: 'expatternprovider:cobblestone_cell'})
-event.remove({ id: 'expattenprovider:ex_inscriber'})
+event.remove({ id: "expatternprovider:cobblestone_cell"})
+event.remove({ id: "expattenprovider:ex_inscriber"})
 
 // Remove In - World Transformation Liquid Recipes
-event.remove({ id: 'ae2:transform/fluix_crystals'})
-event.remove({ id: 'ae2:transform/fluix_crystal'})
-event.remove({ id: 'ae2:transform/flawed_budding_quartz'})
-event.remove({ id: 'ae2:transform/chipped_budding_quartz'})
-event.remove({ id: 'ae2:transform/damaged_budding_quartz'})
-event.remove({ id: 'ae2:transform/certus_quartz_crystals'})
+event.remove({ id: "ae2:transform/fluix_crystals"})
+event.remove({ id: "ae2:transform/fluix_crystal"})
+event.remove({ id: "ae2:transform/flawed_budding_quartz"})
+event.remove({ id: "ae2:transform/chipped_budding_quartz"})
+event.remove({ id: "ae2:transform/damaged_budding_quartz"})
+event.remove({ id: "ae2:transform/certus_quartz_crystals"})
 
 
 // New Charger Recipe
-event.remove({ id: 'ae2:network/blocks/crystal_processing_charger'})
+event.remove({ id: "ae2:network/blocks/crystal_processing_charger"})
 event.shaped(
-  Item.of('ae2:charger'),
+  Item.of("ae2:charger"),
   [
-    'IOI',
-    'S  ',
-    'IOI'
+    "IBI",
+    "S  ",
+    "IBI"
   ],
   {
-    I: 'create_new_age:overcharged_iron',
-    S: 'ae2:printed_silicon',
-    O: 'create_dd:overcharge_alloy'
+    I: "create_new_age:overcharged_iron",
+    S: "ae2:printed_silicon",
+    B: "create_additions_synthetics:dense_brass_sheet"
   }
-).id( 'forge_frontier:shaped/charger')
+).id( "forge_frontier:shaped/charger")
 
 
 // New ME Import Bus Recipe
-event.remove({ id: 'ae2:network/parts/import_bus'})
+event.remove({ id: "ae2:network/parts/import_bus"})
 event.shaped(
-  Item.of('ae2:import_bus'),
+  Item.of("ae2:import_bus"),
   [
-    ' A ',
-    'IPI',
-    ' C '
+    " C ",
+    " P ",
+    "IAI"
   ],
   {
-    A: 'ae2:annihilation_core',
-    I: 'minecraft:iron_ingot',
-    P: 'minecraft:sticky_piston',
-    C: 'create:smart_chute'
+    A: "ae2:annihilation_core",
+    I: "minecraft:iron_ingot",
+    P: "minecraft:sticky_piston",
+    C: "create:smart_chute"
   }
-).id( 'forge_frontier:shaped/import_bus' )
+).id( "forge_frontier:shaped/import_bus" )
 
 // New ME Export Bus Recipe
-event.remove({ id: 'ae2:network/parts/export_bus'})
+event.remove({ id: "ae2:network/parts/export_bus"})
 event.shaped(
-  Item.of('ae2:export_bus'),
+  Item.of("ae2:export_bus"),
   [
-    'IFI',
-    ' P ',
-    ' C '
+    "IFI",
+    " P ",
+    " C "
   ],
   {
-    I: 'minecraft:iron_ingot',
-    F: 'ae2:formation_core',
-    P: 'minecraft:piston',
-    C: 'create:smart_chute'
+    I: "minecraft:iron_ingot",
+    F: "ae2:formation_core",
+    P: "minecraft:piston",
+    C: "create:smart_chute"
   }
-).id( 'forge_frontier:shaped/export_bus' )
+).id( "forge_frontier:shaped/export_bus" )
 
 // New ME Terminal Recipe
-event.remove({ id: 'ae2:network/parts/terminals'})
+event.remove({ id: "ae2:network/parts/terminals"})
 event.custom({
-  type: 'create:mechanical_crafting',
+  type: "create:mechanical_crafting",
   pattern: [
-    'FLA',
-    'PTP'
+    "FLA",
+    "DTD"
   ],
   key: {
-    F: Ingredient.of('ae2:formation_core'),
-    L: Ingredient.of('ae2:logic_processor'),
-    A: Ingredient.of('ae2:annihilation_core'),
-    P: Ingredient.of('#ae2:illuminated_panel'),
-    T: Ingredient.of('create:stock_ticker')
+    F: Ingredient.of("ae2:formation_core"),
+    L: Ingredient.of("ae2:logic_processor"),
+    A: Ingredient.of("ae2:annihilation_core"),
+    D: Ingredient.of("create:display_board"),
+    T: Ingredient.of("create:stock_ticker")
   },
-  result: Ingredient.of('ae2:terminal'),
+  result: Ingredient.of("ae2:terminal"),
   acceptMirrored: false
-}).id('forge_frontier:mechancial_crafting/terminal');
+}).id("forge_frontier:mechancial_crafting/terminal");
 
 // New ME Storage Terminal Recipe
-event.remove({ id: 'ae2:network/parts/terminals_crafting'})
+event.remove({ id: "ae2:network/parts/terminals_crafting"})
 event.custom({
-  type: 'create:mechanical_crafting',
+  type: "create:mechanical_crafting",
   pattern: [
-    'CCC',
-    'PTP',
-    'CCC'
+    "BcB",
+    "PTP",
+    "BCB"
   ],
   key: {
-    C: Ingredient.of('create:mechanical_crafter'),
-    P: Ingredient.of('ae2:calculation_processor'),
-    T: Ingredient.of('ae2:terminal')
+    B: Ingredient.of("create_additions_synthetics:dense_brass_sheet"),
+    c: Ingredient.of("create:crafting_blueprint"),
+    P: Ingredient.of("ae2:calculation_processor"),
+    T: Ingredient.of("ae2:terminal"),
+    C: Ingredient.of("create:mechanical_crafter")
   },
-  result: Ingredient.of('ae2:crafting_terminal'),
+  result: Ingredient.of("ae2:crafting_terminal"),
   acceptMirrored: false
-}).id('forge_frontier:mechancial_crafting/crafting_terminal');
+}).id("forge_frontier:mechancial_crafting/crafting_terminal");
    
 })
