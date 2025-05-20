7c2ea5b5d71dbd82e65f7d6b1954cf823fd749c8
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-05-20 15:20:15 -0500

    Removed duplicate recipes, covered by Quark

--- kubejs/client_scripts/JEI/Remove_Entries.js
+++ kubejs/client_scripts/JEI/Remove_Entries.js
@@ -5,6 +5,17 @@ JEIEvents.hideItems(event =>
         event.hide('@citadel')
         event.hide('@moonlight')
         event.hide('minecraft:painting')
-
+        event.hide('create_dd:crafting_kinetic_mechanism1')
+        event.hide('create_dd:crafting_kinetic_mechanism2')
+        event.hide('create_dd:ponder_in_a_box')
+        event.hide('create_dd:potato_turret')
+        event.hide('create_central_kitchen:pomegranate_milkshake')
+        event.hide('create_central_kitchen:chocolate_milkshake')
+        event.hide('create_central_kitchen:strawberry_milkshake')
+        event.hide('create_central_kitchen:vanilla_milkshake')
+        event.hide('create_central_kitchen:adzuki_milkshake')
+        event.hide('createteleporters:pocket_dimension_remote')
+        event.hide('monolib:debug_item')
+        event.hide('monolib:debug_block')
     }
 )
