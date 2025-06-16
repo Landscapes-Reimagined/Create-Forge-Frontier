919a1e082410744d96ee10956e502d58df603504
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-06-15 21:49:47 -0500

    Added missing recipe ids

--- kubejs/server_scripts/Ad Astra/AdAstra_Tweaks.js
+++ kubejs/server_scripts/Ad Astra/AdAstra_Tweaks.js
@@ -11,7 +11,8 @@ ServerEvents.recipes(event => {
                 item: "ad_astra:iron_plate"
             },
             result: "ad_astra:iron_plating"
-        })
+        }).id('forge_frontier:stonecutting/astra_iron_plating')
+
         // Steel
         event.remove({ id: 'ad_astra:steel_plating' })
         event.custom({
@@ -21,7 +22,8 @@ ServerEvents.recipes(event => {
                 item: "ad_astra:steel_plate"
             },
             result: "ad_astra:steel_plating"
-        })
+        }).id('forge_frontier:stonecutting/astra_steel_plating')
+
         // Desh
         event.remove({ id: 'ad_astra:desh_plating' })
         event.custom({
@@ -31,7 +33,7 @@ ServerEvents.recipes(event => {
                 item: "ad_astra:desh_plate"
             },
             result: "ad_astra:desh_plating"
-        })
+        }).id('forge_frontier:stonecutting/astra_desh_plating')
 
         // Ostrum
         event.remove({ id: 'ad_astra:ostrum_plating' })
@@ -42,7 +44,7 @@ ServerEvents.recipes(event => {
                 item: "ad_astra:ostrum_plate"
             },
             result: "ad_astra:ostrum_plating"
-        })
+        }).id('forge_frontier:stonecutting/astra_ostrum_plating')
 
         // Calorite
         event.remove({ id: 'ad_astra:calorite_plating' })
@@ -53,7 +55,7 @@ ServerEvents.recipes(event => {
                 item: "ad_astra:calorite_plate"
             },
             result: "ad_astra:calorite_plating"
-        })        
+        }).id('forge_frontier:stonecutting/astra_calorite_plating')       
 
     // Recipe Removal
     event.remove({ id: 'ad_astra:recipes/steel_ingot_from_blasting_iron_ingot'})
