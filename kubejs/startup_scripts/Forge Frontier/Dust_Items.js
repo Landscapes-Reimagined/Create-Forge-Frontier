9bb1594ab77f6876e778ae1c85e7c8fcb46be6e3
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-09-15 15:10:28 -0500

    New Clean and Dirty dust for Mettalurgy compat

---
+++ kubejs/startup_scripts/Forge Frontier/Dust_Items.js
@@ -0,0 +1,31 @@
+// Dusts.js
+StartupEvents.registry('item', event => {
+
+    const materials = [
+        'Bismuth', 'Calorite', 'Desh', 'Irradium', 'Malachite', 'Ostrum',
+        'Verdantine', 'Glacium', 'Debris', 'Resonite', 'Aubrum', 'Pyroclast',
+        'Frostite', 'Stellaris', 'Radiantite', 'Shale', 'Palerock', 'Guanite',
+        'Pearlyte', 'Amberlite', 'Azurnium', 'Neodymrium', 'Uraniumnite', 'Sulphite',
+        'Reggarfonite'
+    ];
+
+    function createDirtyDust(event, material) {
+        const id = `dirty_${material.toLowerCase()}_dust`;
+        event.create(`forge_frontier:${id}`)
+            .displayName(`Dirty ${material} Dust`)
+            .texture(`forge_frontier:item/dirty_dust/${id}`);
+    }
+
+    function createCleanDust(event, material) {
+        const id = `${material.toLowerCase()}_dust`;
+        event.create(`forge_frontier:${id}`)
+            .displayName(`${material} Dust`)
+            .texture(`forge_frontier:item/dust/${id}`);
+    }
+
+    materials.forEach(material => {
+        createDirtyDust(event, material);
+        createCleanDust(event, material);
+    });
+
+});
