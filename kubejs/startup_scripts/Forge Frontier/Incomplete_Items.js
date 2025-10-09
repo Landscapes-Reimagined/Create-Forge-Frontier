f742dcbdef313cc93af0e82beefccd04e34b1351
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-10-09 14:57:43 -0500

    Creation of incomplete eye items

---
+++ kubejs/startup_scripts/Forge Frontier/Incomplete_Items.js
@@ -0,0 +1,49 @@
+StartupEvents.registry('item', event => {
+    // Abyss Eye
+    event.create('cataclysm:incomplete_abyss_eye')
+        .displayName('Incomplete Abyss Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_abyss_eye')
+
+    // Cursed Eye
+    event.create('cataclysm:incomplete_cursed_eye')
+        .displayName('Incomplete Cursed Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_cursed_eye')
+
+    // Desert Eye
+    event.create('cataclysm:incomplete_desert_eye')
+        .displayName('Incomplete Desert Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_desert_eye')
+
+    // Flame Eye
+    event.create('cataclysm:incomplete_flame_eye')
+        .displayName('Incomplete Flame Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_flame_eye')
+
+    // Mech Eye
+    event.create('cataclysm:incomplete_mech_eye')
+        .displayName('Incomplete Mech Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_mech_eye')
+
+    // Monstrous Eye
+    event.create('cataclysm:incomplete_monstrous_eye')
+        .displayName('Incomplete Monstrous Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_monstrous_eye')
+
+    // Storm Eye
+    event.create('cataclysm:incomplete_storm_eye')
+        .displayName('Incomplete Storm Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_storm_eye')
+
+    // Void Eye
+    event.create('cataclysm:incomplete_void_eye')
+        .displayName('Incomplete Void Eye')
+        .maxStackSize(1)
+        .texture('forge_frontier:item/incomplete_void_eye')
+});
