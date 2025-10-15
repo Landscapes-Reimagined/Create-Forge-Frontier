c37c5774bcce3fb7bbd07a1ca455f4206d368688
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-10-14 22:52:51 -0500

    Added Missing Bronze Recipes

--- kubejs/server_scripts/Compat/Create Metallurgy/Casting_Table_Recipes.js
+++ kubejs/server_scripts/Compat/Create Metallurgy/Casting_Table_Recipes.js
@@ -36,6 +36,7 @@ ServerEvents.recipes(function (event) {
         { name: "reggarfonite", fluid: "forge_frontier:molten_reggarfonite", ingot: "create_better_motors:reggarfonite_gem", nugget: "create_better_motors:reggarfonite_nugget", plate: "create_better_motors:reggarfonite_sheet", rodTag: "forge:rods/reggarfonite", wireTag: "forge:wires/reggarfonite" },
         { name: "ostrum", fluid: "forge_frontier:molten_ostrum", ingot: "ad_astra:ostrum_ingot", nugget: "ad_astra:ostrum_nugget", plate: "ad_astra:ostrum_plate", rodTag: "forge:rods/ostrum", wireTag: "forge:wires/ostrum" },
         { name: "dielectric_paste", fluid: "forge_frontier:molten_dielectric_paste", ingot: "powah:dielectric_paste", rod: "powah:dielectric_rod", wire: "powah:dielectric_rod_horizontal"},
+        { name: "bronze", fluid: "createmetallurgy:molten_bronze", ingot: "create_dd:bronze_ingot", nugget: "create_dd:bronze_nugget", plate: "create_dd:bronze_sheet"},
 
         // Advanced Netherite tiers
         { name: "netherite_iron", fluid: "forge_frontier:molten_netherite_iron", ingot: "advancednetherite:netherite_iron_ingot", nugget: "advancednetherite:netherite_iron_nugget", plate: "forge_frontier:netherite_iron_sheet", rodTag: "forge:rods/netherite_iron", wireTag: "forge:wires/netherite_iron" },
