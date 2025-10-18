be8a4f742fb872df773eff18c6884f7b5f4b04ad
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2025-10-18 12:43:49 -0500

    Removed Extra recipes, added missing recipes

--- kubejs/server_scripts/Compat/Create Metallurgy/Casting_Table_Recipes.js
+++ kubejs/server_scripts/Compat/Create Metallurgy/Casting_Table_Recipes.js
@@ -7,12 +7,12 @@ ServerEvents.recipes(function (event) {
 
     var tableCasts = [
         // Vanilla + base metals
-        { name: "iron", fluid: "createmetallurgy:molten_iron", ingot: "minecraft:iron_ingot", nugget: "minecraft:iron_nugget", plate: "create:iron_sheet", rodTag: "forge:rods/iron", wireTag: "forge:wires/iron" },
-        { name: "gold", fluid: "createmetallurgy:molten_gold", ingot: "minecraft:gold_ingot", nugget: "minecraft:gold_nugget", plate: "create:golden_sheet", rodTag: "forge:rods/gold", wireTag: "forge:wires/gold" },
-        { name: "copper", fluid: "createmetallurgy:molten_copper", ingot: "minecraft:copper_ingot", nugget: "create:copper_nugget", plate: "create:copper_sheet", rodTag: "forge:rods/copper", wireTag: "forge:wires/copper" },
-        { name: "tin", fluid: "createmetallurgy:molten_tin", ingot: "create_dd:tin_ingot", nugget: "create_dd:tin_nugget", plate: "create_dd:tin_sheet", rodTag: "forge:rods/tin", wireTag: "forge:wires/tin" },
+        { name: "iron", fluid: "createmetallurgy:molten_iron", wireTag: "forge:wires/iron" },
+        { name: "gold", fluid: "createmetallurgy:molten_gold", wireTag: "forge:wires/gold" },
+        { name: "copper", fluid: "createmetallurgy:molten_copper", wireTag: "forge:wires/copper" },
         { name: "debris", fluid: "forge_frontier:molten_debris", ingot: "minecraft:netherite_scrap"},
         { name: "coal", fluid: "forge_frontier:molten_carbon", ingot: "minecraft:coal"},
+        { name: "netherite", fluid: "createmetallurgy:molten_netherite", nugget: "createdeco:netherite_nugget", plate: "createdeco:netherite_sheet"},
 
         // Forge Frontier + special ores
         { name: "bismuth", fluid: "forge_frontier:molten_bismuth", ingot: "enlightened_end:bismuth_ingot", nugget: "enlightened_end:bismuth_nugget", plate: "forge_frontier:bismuth_sheet", rodTag: "forge:rods/bismuth", wireTag: "forge:wires/bismuth" },
@@ -20,7 +20,7 @@ ServerEvents.recipes(function (event) {
         { name: "calorite", fluid: "forge_frontier:molten_calorite", ingot: "ad_astra:calorite_ingot", nugget: "ad_astra:calorite_nugget", plate: "ad_astra:calorite_plate", rodTag: "forge:rods/calorite", wireTag: "forge:wires/calorite" },
         { name: "irradium", fluid: "forge_frontier:molten_irradium", ingot: "enlightened_end:irradium_bar", nugget: "forge_frontier:irradium_nugget", plate: "forge_frontier:irradium_sheet", rodTag: "forge:rods/irradium", wireTag: "forge:wires/irradium" },
         { name: "malachite", fluid: "forge_frontier:molten_malachite", ingot: "enlightened_end:malachite", nugget: "forge_frontier:malachite_nugget", plate: "forge_frontier:malachite_sheet", rodTag: "forge:rods/malachite", wireTag: "forge:wires/malachite" },
-        { name: "etrium", fluid: "forge_frontier:molten_etrium", ingot: "ad_astra:etrium_ingot", nugget: "ad_astra:etrium_nugget", plate: "ad_astra:etrium_plate", rodTag: "forge:rods/etrium", wireTag: "forge:wires/etrium" },
+        { name: "etrium", fluid: "forge_frontier:molten_etrium", ingot: "ad_astra:etrium_ingot", nugget: "ad_astra:etrium_nugget", plate: "ad_astra:etrium_plate", rod: "ad_astra:etrium_rod", wireTag: "forge:wires/etrium" },
         { name: "emerald", fluid: "forge_frontier:molten_emerald", ingot: "minecraft:emerald", nugget: "forge_frontier:emerald_nugget", plate: "forge_frontier:emerald_sheet", rodTag: "forge:rods/emerald", wireTag: "forge:wires/emerald" },
         { name: "diamond", fluid: "forge_frontier:molten_diamond", ingot: "minecraft:diamond", nugget: "forge_frontier:diamond_nugget", plate: "forge_frontier:diamond_sheet", rodTag: "forge:rods/diamond", wireTag: "forge:wires/diamond" },
         { name: "echo", fluid: "forge_frontier:molten_echo_shard", ingot: "minecraft:echo_shard", nugget: "forge_frontier:echo_nugget", plate: "forge_frontier:echo_sheet", rodTag: "forge:rods/echo", wireTag: "forge:wires/echo" },
@@ -35,8 +35,10 @@ ServerEvents.recipes(function (event) {
         { name: "uranium", fluid: "forge_frontier:molten_uranium", ingot: "alexscaves:uranium", nugget: "forge_frontier:uranium_nugget", plate: "forge_frontier:uranium_sheet", rodTag: "forge:rods/uranium", wireTag: "forge:wires/uranium" },
         { name: "reggarfonite", fluid: "forge_frontier:molten_reggarfonite", ingot: "create_better_motors:reggarfonite_gem", nugget: "create_better_motors:reggarfonite_nugget", plate: "create_better_motors:reggarfonite_sheet", rodTag: "forge:rods/reggarfonite", wireTag: "forge:wires/reggarfonite" },
         { name: "ostrum", fluid: "forge_frontier:molten_ostrum", ingot: "ad_astra:ostrum_ingot", nugget: "ad_astra:ostrum_nugget", plate: "ad_astra:ostrum_plate", rodTag: "forge:rods/ostrum", wireTag: "forge:wires/ostrum" },
-        { name: "dielectric_paste", fluid: "forge_frontier:molten_dielectric_paste", ingot: "powah:dielectric_paste", rod: "powah:dielectric_rod", wire: "powah:dielectric_rod_horizontal"},
+        { name: "dielectric_paste", fluid: "forge_frontier:molten_dielectric_paste", ingot: "powah:dielectric_paste", rod: "powah:dielectric_rod"},
         { name: "bronze", fluid: "createmetallurgy:molten_bronze", ingot: "create_dd:bronze_ingot", nugget: "create_dd:bronze_nugget", plate: "create_dd:bronze_sheet"},
+        { name: "tungsten", fluid: "createmetallurgy:molten_tungsten", wire: "createmetallurgy:tungsten_wire"},
+        { name: "electrum", fluid: "createmetallurgy:molten_electrum", wireTag: "forge:wires/electrum"},
 
         // Advanced Netherite tiers
         { name: "netherite_iron", fluid: "forge_frontier:molten_netherite_iron", ingot: "advancednetherite:netherite_iron_ingot", nugget: "advancednetherite:netherite_iron_nugget", plate: "forge_frontier:netherite_iron_sheet", rodTag: "forge:rods/netherite_iron", wireTag: "forge:wires/netherite_iron" },
