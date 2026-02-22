2811a55bdd0c2a697a6ca3190340eb1053e79a91
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-02-22 14:21:59 -0600

    Kubejs Server Script removals so far

--- kubejs/server_scripts/Mod Adjustments/AE2/Recipe_Removals.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes(event => 
-    {
-        event.remove({ id: "expatternprovider:cobblestone_cell"})
-        event.remove({ id: "expattenprovider:ex_inscriber"})
-        event.remove({ id: "ae2:network/blocks/spatial_anchor"})
-    }
-)
--- kubejs/server_scripts/Mod Adjustments/Applied Flux/Recipe_Removals.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes(event => 
-    {
-        // Removes Not Needed Recipes
-            event.remove({ id: 'ad_astra_giselle_addon:compat/ae2/inscriber/sky_stone_dust' })
-            event.remove({ id: 'appflux:transform/redstone_crystal'})
-    }
-)
