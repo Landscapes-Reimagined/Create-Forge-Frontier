07fa6e6b26706cbe1992a87a308cb82d4beed237
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-03-06 22:00:49 -0600

    Reworked Jetpack recipes/assets to be closer to vanilla, and changed Netherite jetpack to Mithril jetpack

--- kubejs/server_scripts/Mod Adjustments/Create Stuff and Additions/Mechanical_Crafting_Recipes.js
+++ kubejs/server_scripts/Mod Adjustments/Create Stuff and Additions/Mechanical_Crafting_Recipes.js
@@ -1,16 +1,16 @@
 ServerEvents.recipes(event => 
 	{
-		// Bronze Jetpack Recipe
+		// Copper Jetpack Recipe
 			event.remove({ id: 'create_sa:copper_propeler_recipe'})
 			event.custom({
-				type: "create:mechanical_crafting",
+				type: 'create:mechanical_crafting',
 				pattern: [
 					' ABA ',
 					'CADAC',
 					'CEFEC'
 				],
 				key: {
-					A: Ingredient.of('create_dd:bronze_ingot').toJson(),
+					A: Ingredient.of('minecraft:copper_ingot').toJson(),
 					B: Ingredient.of('create:cogwheel').toJson(),
 					C: Ingredient.of('create:encased_fan').toJson(),
 					D: Ingredient.of('create_sa:hydraulic_engine').toJson(),
@@ -19,12 +19,54 @@ ServerEvents.recipes(event =>
 				},
 				result: Ingredient.of('create_sa:copper_jetpack_chestplate').toJson(),
 				acceptMirrored: false
-			}).id('forge_frontier:mechanical_crafting/bronze_jetpack');
+			}).id('forge_frontier:mechanical_crafting/copper_jetpack');
 
-		// Mithril Jetpack Recipe
+		// Andesite Jetpack Recipe
 			event.remove({ id: 'create_sa:andesite_jetpack_recipe'})
 			event.custom({
-				type: "create:mechanical_crafting",
+				type: 'create:mechanical_crafting',
+				pattern: [
+					' ABA ',
+					'CADAC',
+					'CEFEC'
+				],
+				key: {
+					A: Ingredient.of('create:andesite_alloy').toJson(),
+					B: Ingredient.of('create:cogwheel').toJson(),
+					C: Ingredient.of('create:encased_fan').toJson(),
+					D: Ingredient.of('create_sa:heat_engine').toJson(),
+					E: Ingredient.of('create_sa:large_fueling_tank').toJson(),
+					F: Ingredient.of('create_sa:copper_jetpack_chestplate').toJson()
+				},
+				result: Ingredient.of('create_sa:andesite_jetpack_chestplate').toJson(),
+				acceptMirrored: false
+			}).id('forge_frontier:mechanical_crafting/andesite_jetpack');
+
+		// Brass Jetpack Recipe
+			event.remove({ id: 'create_sa:brass_jetpack_recipe'})
+			event.custom({
+				type: 'create:mechanical_crafting',
+				pattern: [
+					' ABA ',
+					'CADAC',
+					'CEFEC'
+				],
+				key: {
+					A: Ingredient.of('create:brass_ingot').toJson(),
+					B: Ingredient.of('create:cogwheel').toJson(),
+					C: Ingredient.of('create:encased_fan').toJson(),
+					D: Ingredient.of('create_sa:steam_engine').toJson(),
+					E: Ingredient.of('create_sa:large_fueling_tank').toJson(),
+					F: Ingredient.of('create_sa:andesite_jetpack_chestplate').toJson()
+				},
+				result: Ingredient.of('create_sa:brass_jetpack_chestplate').toJson(),
+				acceptMirrored: false
+			}).id('forge_frontier:mechanical_crafting/brass_jetpack');
+
+		// Mithril Jetpack Recipe
+			event.remove({ id: 'create_sa:netherite_jetpack_recipe'})
+			event.custom({
+				type: 'create:mechanical_crafting',
 				pattern: [
 					' ABA ',
 					'CADAC',
@@ -34,33 +76,12 @@ ServerEvents.recipes(event =>
 					A: Ingredient.of('create_dd:mithril_ingot').toJson(),
 					B: Ingredient.of('create:cogwheel').toJson(),
 					C: Ingredient.of('create:encased_fan').toJson(),
-					D: Ingredient.of('create_sa:heat_engine').toJson(),
-					E: Ingredient.of('create_sa:large_fueling_tank').toJson(),
-					F: Ingredient.of('create_sa:copper_jetpack_chestplate').toJson()
-				},
-				result: Ingredient.of('create_sa:andesite_jetpack_chestplate').toJson(),
-				acceptMirrored: false
-			}).id('forge_frontier:mechanical_crafting/mithril_jetpack');
-
-		// Bismuth Jetpack Recipe
-			event.remove({ id: 'create_sa:brass_jetpack_recipe'})
-			event.custom({
-				type: "create:mechanical_crafting",
-				pattern: [
-					' ABA ',
-					'CADAC',
-					'CEFEC'
-				],
-				key: {
-					A: Ingredient.of('enlightened_end:bismuth_ingot').toJson(),
-					B: Ingredient.of('create:cogwheel').toJson(),
-					C: Ingredient.of('create:encased_fan').toJson(),
 					D: Ingredient.of('create_sa:steam_engine').toJson(),
 					E: Ingredient.of('create_sa:large_fueling_tank').toJson(),
-					F: Ingredient.of('create_sa:andesite_jetpack_chestplate').toJson()
+					F: Ingredient.of('create_sa:brass_jetpack_chestplate').toJson()
 				},
-				result: Ingredient.of('create_sa:brass_jetpack_chestplate').toJson(),
+				result: Ingredient.of('create_sa:netherite_jetpack_chestplate').toJson(),
 				acceptMirrored: false
-			}).id('forge_frontier:mechanical_crafting/bismuth_jetpack');
+			}).id('forge_frontier:mechanical_crafting/mithril_jetpack');		
 	}
 )
