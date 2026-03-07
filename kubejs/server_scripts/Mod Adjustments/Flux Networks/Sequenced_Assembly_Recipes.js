fa50368e4185c62ee8f8e505b9d6fec8763f587c
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-03-06 23:45:32 -0600

    Fixed new flux dust recipe

--- kubejs/server_scripts/Mod Adjustments/Flux Networks/Sequenced_Assembly_Recipes.js
+++ kubejs/server_scripts/Mod Adjustments/Flux Networks/Sequenced_Assembly_Recipes.js
@@ -3,29 +3,29 @@ ServerEvents.recipes(event =>
         // Flux Dust Recipe
         event.remove({ id: 'fluxnetworks:flux_dust' })
         event.custom({
-            type: "create:sequenced_assembly",
-            ingredient: { item: "create:obsidian_dust" },
-            transitionalItem: { item: "forge_frontier:incomplete_flux_dust" },
+            type: 'create:sequenced_assembly',
+            ingredient: { item: 'create:powdered_obsidian' },
+            transitionalItem: { item: 'forge_frontier:incomplete_flux_dust' },
             loops: 1,
-            results: [{ item: "fluxnetworks:flux_dust" }],
+            results: [{ item: 'fluxnetworks:flux_dust' }],
             sequence: [
                 {
-                    type: "create:deploying",
+                    type: 'create:deploying',
                     ingredients: [
-                        { item: "create:obsidian_dust" },
-                        { item: "create_additions_synthetics:brass_crystal" }
+                        { item: 'create:powdered_obsidian' },
+                        { item: 'create_additions_synthetics:brass_crystal' }
                     ],
-                    results: [{ item: "forge_frontier:incomplete_flux_dust" }]
+                    results: [{ item: 'forge_frontier:incomplete_flux_dust' }]
                 },
                 {
-                    type: "create:pressing",
-                    ingredients: [{ item: "forge_frontier:incomplete_flux_dust" }],
-                    results: [{ item: "forge_frontier:incomplete_flux_dust" }]
+                    type: 'create:pressing',
+                    ingredients: [{ item: 'forge_frontier:incomplete_flux_dust' }],
+                    results: [{ item: 'forge_frontier:incomplete_flux_dust' }]
                 },
                 {
-                    type: "create:pressing",
-                    ingredients: [{ item: "forge_frontier:incomplete_flux_dust" }],
-                    results: [{ item: "fluxnetworks:flux_dust" }]
+                    type: 'create:pressing',
+                    ingredients: [{ item: 'forge_frontier:incomplete_flux_dust' }],
+                    results: [{ item: 'fluxnetworks:flux_dust' }]
                 }
             ]
         }).id('forge_frontier:sequenced_assembly/flux_dust')
