d2b275683a0c960bc7afdf5ba6beccb280f1489a
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-03-14 02:04:24 -0500

    Added Toms Simple Storage to Create Wrenchable tag

--- kubejs/server_scripts/Compat/Tags/Block_Tags.js
+++ kubejs/server_scripts/Compat/Tags/Block_Tags.js
@@ -19,6 +19,20 @@ ServerEvents.tags('block', event =>
         
         // Create Wrenchable
             event.add('create:wrench_pickup', [
+                "toms_storage:ts.crafting_terminal",
+                "toms_storage:ts.inventory_cable_connector_filtered",
+                "toms_storage:ts.inventory_cable_connector_framed",
+                "toms_storage:ts.inventory_cable_connector",
+                "toms_storage:ts.inventory_cable_framed",
+                "toms_storage:ts.inventory_cable",
+                "toms_storage:ts.inventory_connector",
+                "toms_storage:ts.inventory_hopper_basic",
+                "toms_storage:ts.inventory_proxy",
+                "toms_storage:ts.level_emitter",
+                "toms_storage:ts.open_crate",
+                "toms_storage:ts.painted_trim",
+                "toms_storage:ts.storage_terminal",
+                "toms_storage:ts.trim",
                 "minecraft:copper_door",
                 "minecraft:exposed_copper_door",
                 "minecraft:weathered_copper_door",
