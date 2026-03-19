783c901da5b99b34cd8247682635e979f76ac402
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-03-19 18:13:31 -0500

    Reformatted Templates script to make it easier for new additions

--- kubejs/startup_scripts/Forge Frontier/Templates.js
+++ kubejs/startup_scripts/Forge Frontier/Templates.js
@@ -1,159 +1,45 @@
-StartupEvents.registry('item', event => 
-    {
+StartupEvents.registry('item', event => {
+    const items = [
         // Incomplete Templates
-            // Incomplete Netherite Upgrade
-                event.create('forge_frontier:incomplete_netherite_upgrade')
-                    .displayName('Incomplete Netherite Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_netherite_upgrade")
-
-            // Incomplete mysterious Upgrade
-                event.create('forge_frontier:incomplete_mysterious_upgrade')
-                    .displayName('Incomplete Mysterious Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_mysterious_upgrade")
-            
-            // Incomplete curious Upgrade
-                event.create('forge_frontier:incomplete_curious_upgrade')
-                    .displayName('Incomplete Curious Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_curious_upgrade")
-
-            // Incomplete Ancient Armor Trim
-                event.create('forge_frontier:incomplete_ancient_armor_trim')
-                    .displayName('Incomplete Ancient Armor Trim')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_ancient_armor_trim")
-
-            // Incomplete Adamantite Template
-                event.create('forge_frontier:incomplete_adamantite_template')
-                    .displayName('Incomplete Adamantite Template')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_adamantite_template")
-
-            // Incomplete Pearlescent Upgrade
-                event.create('forge_frontier:incomplete_pearlescent_upgrade')
-                    .displayName('Incomplete Pearlescent Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_pearlescent_upgrade")            
-
-            // Incomplete Ethium Upgrade
-                event.create('forge_frontier:incomplete_ethium_upgrade')
-                    .displayName('Incomplete Ethium Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_ethium_upgrade")            
-
-            // Incomplete Polarity Armor Trim
-                event.create('forge_frontier:incomplete_polarity_armor_trim')
-                    .displayName('Incomplete Polarity Armor Trim')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_polarity_armor_trim")
-            
-            // Incomplete Goggle Armor Trim
-                event.create('forge_frontier:incomplete_goggle_armor_trim')
-                    .displayName('Incomplete Goggle Armor Trim')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_goggle_armor_trim")            
-
-            // Incomplete Runic Etching
-                event.create('forge_frontier:incomplete_runic_etching')
-                    .displayName('Incomplete Runic Etching')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_runic_etching")               
-
-            // Incomplete Primal Armor Trim
-                event.create('forge_frontier:incomplete_primal_armor_trim')
-                    .displayName('Incomplete Primal Armor Trim')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_primal_armor_trim")             
-
-            // Incomplete Ignitium Upgrade
-                event.create('forge_frontier:incomplete_ignitium_upgrade')
-                    .displayName('Incomplete Ignitium Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_ignitium_upgrade")                
-
-            // Incomplete Cursium Upgrade
-                event.create('forge_frontier:incomplete_cursium_upgrade')
-                    .displayName('Incomplete Cursium Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_cursium_upgrade")  
-
-            // Incomplete Warden Upgrade
-                event.create('forge_frontier:incomplete_warden_upgrade')
-                    .displayName('Incomplete Warden Upgrade')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_warden_upgrade")
-
-            // Incomplete Bolt Armor Trim
-                event.create('forge_frontier:incomplete_bolt_template')
-                    .displayName('Incomplete Bolt Armor Trim')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_bolt_template")
-
-            // Incomplete Flow Armor Trim
-                event.create('forge_frontier:incomplete_flow_template')
-                    .displayName('Incomplete Flow Armor Trim')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/incomplete_items/incomplete_flow_template")
+        { id: 'forge_frontier:incomplete_netherite_upgrade', name: 'Incomplete Netherite Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_netherite_upgrade' },
+        { id: 'forge_frontier:incomplete_mysterious_upgrade', name: 'Incomplete Mysterious Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_mysterious_upgrade' },
+        { id: 'forge_frontier:incomplete_curious_upgrade', name: 'Incomplete Curious Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_curious_upgrade' },
+        { id: 'forge_frontier:incomplete_ancient_armor_trim', name: 'Incomplete Ancient Armor Trim', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_ancient_armor_trim' },
+        { id: 'forge_frontier:incomplete_adamantite_template', name: 'Incomplete Adamantite Template', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_adamantite_template' },
+        { id: 'forge_frontier:incomplete_pearlescent_upgrade', name: 'Incomplete Pearlescent Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_pearlescent_upgrade' },
+        { id: 'forge_frontier:incomplete_ethium_upgrade', name: 'Incomplete Ethium Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_ethium_upgrade' },
+        { id: 'forge_frontier:incomplete_polarity_armor_trim', name: 'Incomplete Polarity Armor Trim', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_polarity_armor_trim' },
+        { id: 'forge_frontier:incomplete_goggle_armor_trim', name: 'Incomplete Goggle Armor Trim', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_goggle_armor_trim' },
+        { id: 'forge_frontier:incomplete_runic_etching', name: 'Incomplete Runic Etching', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_runic_etching' },
+        { id: 'forge_frontier:incomplete_primal_armor_trim', name: 'Incomplete Primal Armor Trim', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_primal_armor_trim' },
+        { id: 'forge_frontier:incomplete_ignitium_upgrade', name: 'Incomplete Ignitium Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_ignitium_upgrade' },
+        { id: 'forge_frontier:incomplete_cursium_upgrade', name: 'Incomplete Cursium Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_cursium_upgrade' },
+        { id: 'forge_frontier:incomplete_warden_upgrade', name: 'Incomplete Warden Upgrade', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_warden_upgrade' },
+        { id: 'forge_frontier:incomplete_bolt_template', name: 'Incomplete Bolt Armor Trim', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_bolt_template' },
+        { id: 'forge_frontier:incomplete_flow_template', name: 'Incomplete Flow Armor Trim', stack: 64, texture: 'forge_frontier:item/incomplete_items/incomplete_flow_template' },
 
         // Custom Shards
-            // Copper Shard
-                    event.create('forge_frontier:copper_shard')
-                    .displayName('Copper Shard')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/copper_shard")
-
-            // Breeze Shard
-                    event.create('forge_frontier:breeze_shard')
-                    .displayName('Breeze Shard')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/breeze_shard")
-
-            // Iron Shard
-                    event.create('forge_frontier:iron_shard')
-                    .displayName('Iron Shard')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/iron_shard")
-
-            // Gold Shard
-                event.create('forge_frontier:gold_shard')
-                    .displayName('Gold Shard')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/gold_shard")    
-
-            // Emerald Shard
-                event.create('forge_frontier:emerald_shard')
-                    .displayName('Emerald Shard')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/emerald_shard")
-
-            // Netherite Shard
-                event.create('forge_frontier:netherite_shard')
-                    .displayName('Netherite Shard')
-                    .maxStackSize(64)
-                    .texture("forge_frontier:item/netherite_shard")
+        { id: 'forge_frontier:copper_shard', name: 'Copper Shard', stack: 64, texture: 'forge_frontier:item/copper_shard' },
+        { id: 'forge_frontier:breeze_shard', name: 'Breeze Shard', stack: 64, texture: 'forge_frontier:item/breeze_shard' },
+        { id: 'forge_frontier:iron_shard', name: 'Iron Shard', stack: 64, texture: 'forge_frontier:item/iron_shard' },
+        { id: 'forge_frontier:gold_shard', name: 'Gold Shard', stack: 64, texture: 'forge_frontier:item/gold_shard' },
+        { id: 'forge_frontier:emerald_shard', name: 'Emerald Shard', stack: 64, texture: 'forge_frontier:item/emerald_shard' },
+        { id: 'forge_frontier:netherite_shard', name: 'Netherite Shard', stack: 64, texture: 'forge_frontier:item/netherite_shard' },
 
         // Netherite Upgrades
-            // Netherite Iron
-                event.create('forge_frontier:netherite_iron_upgrade')
-                    .displayName('Netherite Iron Smithing Template')
-                    .texture('forge_frontier:item/upgrades/netherite_iron_upgrade_smithing_template')
+        { id: 'forge_frontier:netherite_iron_upgrade', name: 'Netherite Iron Smithing Template', texture: 'forge_frontier:item/upgrades/netherite_iron_upgrade_smithing_template' },
+        { id: 'forge_frontier:netherite_gold_upgrade', name: 'Netherite Gold Smithing Template', texture: 'forge_frontier:item/upgrades/netherite_gold_upgrade_smithing_template' },
+        { id: 'forge_frontier:netherite_emerald_upgrade', name: 'Netherite Emerald Smithing Template', texture: 'forge_frontier:item/upgrades/netherite_emerald_upgrade_smithing_template' },
+        { id: 'forge_frontier:netherite_diamond_upgrade', name: 'Netherite Diamond Smithing Template', texture: 'forge_frontier:item/upgrades/netherite_diamond_upgrade_smithing_template' }
+    ];
 
-            // Netherite Gold
-                event.create('forge_frontier:netherite_gold_upgrade')
-                    .displayName('Netherite Gold Smithing Template')
-                    .texture('forge_frontier:item/upgrades/netherite_gold_upgrade_smithing_template')
+    items.forEach(item => {
+        let created = event.create(item.id)
+            .displayName(item.name)
+            .texture(item.texture);
 
-            // Netherite Emerald
-                event.create('forge_frontier:netherite_emerald_upgrade')
-                    .displayName('Netherite Emerald Smithing Template')
-                    .texture('forge_frontier:item/upgrades/netherite_emerald_upgrade_smithing_template')
-
-            // Netherite Diamond    
-                event.create('forge_frontier:netherite_diamond_upgrade')
-                    .displayName('Netherite Diamond Smithing Template')
-                    .texture('forge_frontier:item/upgrades/netherite_diamond_upgrade_smithing_template')
-
-    }
-)
+        if (item.stack !== undefined) {
+            created.maxStackSize(item.stack);
+        }
+    });
+});
