945f43f11e8dc4e30458dabff0236899973ee8cf
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-05-17 19:50:15 -0500

    New Ethium Recipe
    Replaced Nether Stars with Dragon Charges

--- kubejs/server_scripts/Mod Adjustments/Create Ethium/Mixing_Recipes.js
+++ kubejs/server_scripts/Mod Adjustments/Create Ethium/Mixing_Recipes.js
@@ -1,5 +1,39 @@
 ServerEvents.recipes(event => {
 
+    // New Ethium Recipe
+    event.remove({ id: "create_ethium:ethiumrecipe"})
+    event.custom({
+      type: "create:mixing",
+      ingredients: [
+        {
+          item: "create_ethium:polished_stellarite"
+        },
+        {
+          fluid: "create_ethium:echo_compound_fluid",  
+          amount: 125 
+        },
+        {
+          fluid: "create_ethium:chorus_nectar_fluid",
+          amount: 125
+        },
+        {
+          item: "minecraft:netherite_ingot"
+        },
+        {
+          item: "create:brass_ingot"
+        },
+        {
+          item: "amendments:dragon_charge"
+        }
+      ],
+      results: [
+        {
+          item: "create_ethium:ethium_ingot",
+        }
+      ],
+      heatRequirement: "superheated"
+    }).id('forge_frontier:mixing/ethium_ingot');
+    
     // New Amethyst Mix Mixing Recipe
     event.custom({
       type: "create:mixing",
