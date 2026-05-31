ee5c16ab6720c52b7b86b4b5fe74e08ed2143876
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-05-31 06:26:16 -0500

    Revert "Removed Addon Catalysts from JEI for mods we don't have"
    This reverts commit 0fd3b645565790bd9eca7f9ba3f2ba460f5507dc.

--- kubejs/server_scripts/Mod Adjustments/Create Connected/Item_Application_Recipes.js
+++ kubejs/server_scripts/Mod Adjustments/Create Connected/Item_Application_Recipes.js
@@ -17,42 +17,5 @@ ServerEvents.recipes(event =>
                 }
                 ]
             }).id('forge_frontier:item_application/dragon_head_catalyst');
-
-        // Freezing Catalyst
-            event.custom({
-                type: 'create:item_application',
-                ingredients: [
-                {
-                    item: 'create_connected:empty_fan_catalyst'
-                },
-                {
-                    item: "minecraft:powder_snow_bucket"
-                }
-                ],
-                results: [
-                {
-                    item: "create_connected:fan_freezing_catalyst"
-                }
-                ]
-            }).id('forge_frontier:item_application/freezing_catalyst');        
-
-        // Seething Catalyst
-            event.custom({
-                type: 'create:item_application',
-                ingredients: [
-                {
-                    item: 'create_connected:empty_fan_catalyst'
-                },
-                {
-                    item: "create:blaze_cake"
-                }
-                ],
-                results: [
-                {
-                    item: "create_connected:fan_seething_catalyst"
-                }
-                ]
-            }).id('forge_frontier:item_application/seething_catalyst'); 
-
     }
 )
