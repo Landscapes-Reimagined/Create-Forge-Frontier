aa6adf16891d2b6133ecf21686bce554877a2bc5
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-06-10 18:44:07 -0500

    Removed Copper Age Backport, no longer works and likely abandoned

--- kubejs/server_scripts/Mod Adjustments/Friends and Foes/Recipe_Removals.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.recipes(event=>
-    {
-        event.remove({ id: 'friendsandfoes:copper_button' })
-        event.remove({ id: 'friendsandfoes:waxed_copper_button_from_honeycomb' })
-        event.remove({ id: 'friendsandfoes:waxed_exposed_copper_button_from_honeycomb' })
-        event.remove({ id: 'friendsandfoes:waxed_weathered_copper_button_from_honeycomb' })
-        event.remove({ id: 'friendsandfoes:waxed_oxidized_copper_button_from_honeycomb' })
-    }
-)
--- kubejs/startup_scripts/Forge Frontier/Mod_Renaming.js
+++ kubejs/startup_scripts/Forge Frontier/Mod_Renaming.js
@@ -1,2 +1,1 @@
 Platform.mods.trials.name = 'Minecraft'
-Platform.mods.copperagebackport.name = 'Minecraft'
