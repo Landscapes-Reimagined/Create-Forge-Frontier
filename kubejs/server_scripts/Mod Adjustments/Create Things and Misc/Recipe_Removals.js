cc080041edda935d0fc907d93a71dd49c5b5ed0b
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-07-17 19:51:28 -0500

    Remove slime recipe, using Create Fluid slime for all their recipes

--- kubejs/server_scripts/Mod Adjustments/Create Things and Misc/Recipe_Removals.js
+++ kubejs/server_scripts/Mod Adjustments/Create Things and Misc/Recipe_Removals.js
@@ -3,5 +3,6 @@ ServerEvents.recipes(event=>
         event.remove({ id: 'create_things_and_misc:mangrove_sail_craft_backport' })
         event.remove({ id: 'create_things_and_misc:raboutan_sail_c_raft' })
         event.remove({ id: 'create_things_and_misc:jaboticaba_sail_craft' })
+        event.remove({ id: 'create_things_and_misc:slimefluidcraft' })
     }
 )
