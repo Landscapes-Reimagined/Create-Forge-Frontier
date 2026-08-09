5b71b9011044e8b76f6b4c51565dcd3f32bf21e3
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-08-09 18:03:46 -0500

    New Vanilla Compat Recipes

--- kubejs/server_scripts/Compat/Minecraft/Shaped_Recipes.js
+++ kubejs/server_scripts/Compat/Minecraft/Shaped_Recipes.js
@@ -15,5 +15,32 @@ ServerEvents.recipes(event =>
             }
             ).id('forge_frontier:shaped/ender_chest');
 
+        // Compat Bookshelf Recipe
+            event.shaped(
+                Item.of('minecraft:bookshelf'),
+                [
+                    'PPP',
+                    'BBB',
+                    'PPP'
+                ],
+                {
+                    P: "#minecraft:planks",
+                    B: "minecraft:book"
+                }
+            ).id('forge_frontier:shaped/bookshelf')
+
+        // Compat Beehive Recipe
+            event.shaped(
+                Item.of("minecraft:beehive"),
+                [
+                    'PPP',
+                    'HHH',
+                    'PPP'
+                ],
+                {
+                    P: "#minecraft:planks",
+                    H: "minecraft:honeycomb"
+                }
+            ).id('forge_frontier:shaped/beehive')
     }
 )
