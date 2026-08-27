5fdb0201ced74c16e0cc39630330857682fa2c6f
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-08-27 05:33:47 -0500

    Removed old recipe removals

--- kubejs/server_scripts/Mod Adjustments/Dreams and Desires/Recipe_Removals.js
+++ kubejs/server_scripts/Mod Adjustments/Dreams and Desires/Recipe_Removals.js
@@ -1,8 +1,6 @@
 ServerEvents.recipes(event => 
     {
         // Sanding Catalyst Recipe Removals
-            event.remove({ id: 'create_connected:item_application/sanding_castlyst_from_empty_using_deployer' })
-            event.remove({ id: 'create_connected:item_application/sanding_catalyst_from_empty'})
             event.remove({ id: 'create_dd:compacting/steel_ingot'})
             event.remove({ id: 'create_dd:crushing/cobble_asurine'})
             event.remove({ id: 'create_dd:crushing/cobble_crimsite'})
