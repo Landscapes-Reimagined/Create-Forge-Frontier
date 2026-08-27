81407fca29e397a60be540b41553fd02c5bc4380
M0nkeyPr0grammer <86218191+M0nkeyPr0grammer@users.noreply.github.com>
2026-08-27 07:05:38 -0500

    New Custom Tooltips

---
+++ kubejs/client_scripts/Apotheosis/Tooltip.js
@@ -0,0 +1,8 @@
+ItemEvents.tooltip(event => {
+
+    event.add(
+        "apotheosis:boss_summoner",
+        Text.lightPurple('Summons an Apotheosis Boss based on the current dimension tier.')
+    )
+
+})
